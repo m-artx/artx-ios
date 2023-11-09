276b7c33d0f957907796aad550df1be65d683cdc
hchaehyun <hchaehyun@naver.com>
2023-11-10 00:38:03 +0900

    ARTX-57 feat: loginpage 생성, 앱바 연결

--- lib/custom_app_bar.dart
+++ lib/custom_app_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'login.dart'; // 필요한 login.dart 페이지를 import
+// import 'sing_up.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
@@ -7,19 +9,36 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: Color.fromARGB(255, 23, 23, 23),
       leading: IconButton(
         icon: Icon(Icons.menu),
-        onPressed: () => Scaffold.of(context).openDrawer(),  // 메뉴 버튼을 클릭하면 Drawer를 엽니다.
+        onPressed: () => Scaffold.of(context).openDrawer(),
       ),
-      centerTitle: true,  // 앱바의 타이틀을 중앙에 배치
+      centerTitle: true,
       title: Image.asset(
         'assets/images/app_bar_logo.png',
         fit: BoxFit.cover,
-        height: kToolbarHeight * 0.3,  // 로고 이미지 높이를 줄임
+        height: kToolbarHeight * 0.3,
       ),
       actions: [
-        Icon(Icons.person),
+        IconButton(  // person 아이콘에 IconButton을 사용합니다.
+          icon: Icon(Icons.person),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LoginPage()),  // LoginPage로 이동하는 로직을 추가
+            );
+          },
+        ),
         SizedBox(width: 20),
-        Icon(Icons.notifications),
-        SizedBox(width: 10),  // 알림 아이콘과 화면 끝 사이에 여백을 추가
+        /* IconButton(  // person 아이콘에 IconButton을 사용합니다.
+          icon: Icon(Icons.notifications),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SignUpScreenUI()),  // LoginPage로 이동하는 로직을 추가
+            );
+          },
+        ),
+        */
+        SizedBox(width: 10),
       ],
     );
   }
